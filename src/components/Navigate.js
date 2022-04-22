08f03bf68cb74ce1ce45dc047c7aa0e78d6f360b
Michael Hranek <mhranek@comcast.net>
2022-04-21 21:40:39 -0400

    Added Shadowlands talent framing

--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -4,26 +4,26 @@ import React from "react";
 function Navigate({ currentPage, handlePageChange }) {
   return (
     <nav className="pt-5 border-bottom border-5 border-info fs-4">
-      <button 
-      href="#Home"
-      onClick={() => handlePageChange('Home')}
-      className="btn-">Home</button>
-      <button 
-      href="#Dragonflight"
-      onClick={() => handlePageChange('Dragonflight')}
-      className="btn-">Dragonflight</button>
-      <button 
-      href="#TBC"
-      onClick={() => handlePageChange('TBC')}
-      className="btn-">TBC</button>
-      <button 
-      href="#WOTLK"
-      onClick={() => handlePageChange('WOTLK')}
-      className="btn-">WOTLK</button>
-      <button 
-      href="#Shadowlands"
-      onClick={() => handlePageChange('Shadowlands')}
-      className="btn-">Shadowlands</button>
+      <button
+        href="#Home"
+        onClick={() => handlePageChange('Home')}
+        className="btn-">Home</button>
+      <button
+        href="#Dragonflight"
+        onClick={() => handlePageChange('Dragonflight')}
+        className="btn-">Dragonflight</button>
+      <button
+        href="#TBC"
+        onClick={() => handlePageChange('TBC')}
+        className="btn-">TBC</button>
+      <button
+        href="#WOTLK"
+        onClick={() => handlePageChange('WOTLK')}
+        className="btn-">WOTLK</button>
+      <button
+        href="#Shadowlands"
+        onClick={() => handlePageChange('Shadowlands')}
+        className="btn-">Shadowlands</button>
     </nav>
   )
 }
--- src/components/pages/Shadowlands.js
+++ src/components/pages/Shadowlands.js
@@ -1,8 +1,64 @@
 import React from "react";
+import './Shadowlands.css';
+import Col from '../../utils/Col';
+import Row from '../../utils/Row';
 
-function Shadowlands({}) {
+function Shadowlands({ }) {
+  // TODO class picker
+
+  // TODO Tiers of talent rows
+  const talentPane = () => {
+    return <div>
+      <div className="row">
+        <div className="col levelColumn">15</div>
+        <div className="col talentChoice">1-1</div>
+        <div className="col talentChoice">1-2</div>
+        <div className="col talentChoice">1-3</div>
+      </div>
+      <div className="row">
+        <div className="col levelColumn">25</div>
+        <div className="col talentChoice">2-1</div>
+        <div className="col talentChoice">2-2</div>
+        <div className="col talentChoice">2-3</div>
+      </div>
+      <div className="row">
+        <div className="col levelColumn">30</div>
+        <div className="col talentChoice">3-1</div>
+        <div className="col talentChoice">3-2</div>
+        <div className="col talentChoice">3-3</div>
+      </div>
+      <div className="row">
+        <div className="col levelColumn">35</div>
+        <div className="col talentChoice">4-1</div>
+        <div className="col talentChoice">4-2</div>
+        <div className="col talentChoice">4-3</div>
+      </div>
+      <div className="row">
+        <div className="col levelColumn">40</div>
+        <div className="col talentChoice">5-1</div>
+        <div className="col talentChoice">5-2</div>
+        <div className="col talentChoice">5-3</div>
+      </div>
+      <div className="row">
+        <div className="col levelColumn">45</div>
+        <div className="col talentChoice">6-1</div>
+        <div className="col talentChoice">6-2</div>
+        <div className="col talentChoice">6-3</div>
+      </div>
+      <div className="row">
+        <div className="col levelColumn">50</div>
+        <div className="col talentChoice">7-1</div>
+        <div className="col talentChoice">7-2</div>
+        <div className="col talentChoice">7-3</div>
+      </div>
+    </div>
+  }
+
   return (
-    <h1>Shadowlands Page Placeholder</h1>
+    <div className="row">
+      <h1>Shadowlands Talents</h1>
+      {talentPane()}
+    </div>
   )
 }
 
